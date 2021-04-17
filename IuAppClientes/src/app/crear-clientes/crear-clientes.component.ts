0c81ca7cb41e7f8e6d67aa13f6d2228e9135c97c
nacardozog <nacardozog@heon.com.co>
2021-04-17 13:52:04 -0500

    Crear componente de registro de clientes

---
+++ IuAppClientes/src/app/crear-clientes/crear-clientes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-clientes',
+  templateUrl: './crear-clientes.component.html',
+  styleUrls: ['./crear-clientes.component.scss']
+})
+export class CrearClientesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
