fba602814ac61d762eac4049721bcee05ab4e677
nacardozog <nacardozog@heon.com.co>
2021-04-17 16:52:05 -0500

    Desarolllo de la parte de clientes

--- IuAppClientes/src/app/cliente/cliente.component.ts
+++ IuAppClientes/src/app/cliente/cliente.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ServicioGeneralService } from '../servicios/servicio-general.service';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-cliente',
@@ -6,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cliente.component.scss']
 })
 export class ClienteComponent implements OnInit {
-
-  constructor() { }
+  listaClientesD:any=[]
+  constructor(private http: HttpClient, public servicioGeneral: ServicioGeneralService) {
+    this.listaClientes()
+  }
 
   ngOnInit() {
+  }
+  listaClientes() {
+    this.http.get(environment.urlApi + '/listaclientes').subscribe(
+      (data: Array<any>) => {
+        this.listaClientesD=data
+      }, error => {
+        console.log(error);
+        // this.servicioGeneral.MostrarModal('Error', ' Por favor vericar', 'error', 'Aceptar')
+      });
+  }
+  actualizarCliente(item){
+    this.servicioGeneral.listaActualizar=item;
+  }
+  limpiarVariable(){
+    this.servicioGeneral.listaActualizar=[]
   }
 
 }
