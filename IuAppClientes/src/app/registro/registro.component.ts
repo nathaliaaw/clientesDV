de1039d94e0dc4c6003de7272dce07c29612ea73
nacardozog <nacardozog@heon.com.co>
2021-04-17 12:57:59 -0500

    Se ajusta guardado de la informacion del registro de un usuario

--- IuAppClientes/src/app/registro/registro.component.ts
+++ IuAppClientes/src/app/registro/registro.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import {HttpClient} from '@angular/common/http';
-import{ environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
 
 
 @Component({
@@ -12,13 +13,13 @@ import{ environment } from '../../environments/environment';
 export class RegistroComponent implements OnInit {
 
   formRegistro: FormGroup;
-  tipodocumento: string = ''  
+  tipodocumento: string = ''
   numeroDocumento: string = ''
-  completosNombre: string = ''    
+  completosNombre: string = ''
   usuario: string = ''
-  clave: string = ''     
-  listaTipoIdentificacion:any=[]
-  constructor(private http: HttpClient) { 
+  clave: string = ''
+  listaTipoIdentificacion: any = []
+  constructor(private http: HttpClient) {
     this.cargarTipoIdentificacion()
   }
 
@@ -27,18 +28,16 @@ export class RegistroComponent implements OnInit {
     this.formRegistro = new FormGroup({
       tipodocumento: new FormControl('', [Validators.required]),
       numeroDocumento: new FormControl('', [Validators.required]),
-      completosNombre: new FormControl('', [Validators.required]),    
+      completosNombre: new FormControl('', [Validators.required]),
       usuario: new FormControl('', [Validators.required]),
-      clave: new FormControl('', [Validators.required]),      
+      clave: new FormControl('', [Validators.required]),
     });
   }
-  cargarTipoIdentificacion(){
-    
-    const info2 = { }    
+  cargarTipoIdentificacion() {
     this.http.get(environment.urlApi + '/tiposIdentificacion').subscribe(
       (data: Array<any>) => {
-        
-        this.listaTipoIdentificacion=data
+
+        this.listaTipoIdentificacion = data
         // this.datosfechaNoG = data;
         console.log(data);
       }, error => {
@@ -46,17 +45,22 @@ export class RegistroComponent implements OnInit {
       });
   }
   regsitrarUsuario() {
-    if (this.formRegistro.invalid)
-      return;
+    if (this.formRegistro.invalid) 
+    return;
+      // this.servicioGeneral.MostrarModal('Error', ' Por favor ingresar todoos los datos', 'error', 'Aceptar')
+    
 
-    const info2 = { usuario: this.usuario, clave: this.clave }
-    this.http.post(environment.urlApi + '/consultaLogin', info2).subscribe(
+    const info2 = {
+      usuario: this.usuario, clave: this.clave, tipodocumento: this.tipodocumento
+      , numeroDocumento: this.numeroDocumento, completosNombre: this.completosNombre
+    }
+    this.http.post(environment.urlApi + '/insertarLogin', info2).subscribe(
       (data: Array<any>) => {
-        data
-        // this.datosfechaNoG = data;
-        console.log(data);
+        // this.servicioGeneral.MostrarModal('Exitoso', 'Se registro correctamente', 'success', 'Aceptar')
+
       }, error => {
         console.log(error);
+        // this.servicioGeneral.MostrarModal('Error', ' Por favor vericar', 'error', 'Aceptar')
       });
   }
 }
