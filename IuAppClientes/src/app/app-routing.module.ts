c2890405d009579f0fd27360bc88c9b129af43e4
nacardozog <nacardozog@heon.com.co>
2021-04-17 17:01:56 -0500

    Ajuste de ruta de reportes

--- IuAppClientes/src/app/app-routing.module.ts
+++ IuAppClientes/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { ClienteComponent } from './cliente/cliente.component';
 import { CrearClientesComponent } from './crear-clientes/crear-clientes.component';
 import { LoginComponent } from './login/login.component';
 import { RegistroComponent } from './registro/registro.component';
+import { ReportesComponent } from './reportes/reportes.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
@@ -12,6 +13,7 @@ const routes: Routes = [
   { path: 'clientes', component: ClienteComponent },
   { path: 'registro', component: RegistroComponent },
   { path: 'crearClientes', component: CrearClientesComponent },
+  { path: 'reportes', component: ReportesComponent },
 ];
 
 
