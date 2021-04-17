bc47d1f3da398c6022779fdaeddf84ca30046b00
nacardozog <nacardozog@heon.com.co>
2021-04-17 12:58:18 -0500

    Se comenta por el momento la funcion para revizarla mas tarde

--- IuAppClientes/src/app/servicios/servicio-general.service.ts
+++ IuAppClientes/src/app/servicios/servicio-general.service.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@angular/core';
-import swal from 'sweetalert2'
+// import swal from 'sweetalert2'
 @Injectable({
   providedIn: 'root'
 })
 export class ServicioGeneralService {
 
   constructor() { }
-  MostrarModal(titulo, mensaje, icono, textoBoton) {
-    swal.fire({
-      title: titulo,
-      text: mensaje,
-      icon: icono,
-      confirmButtonText: textoBoton
-    })
-  }
+  // MostrarModal(titulo, mensaje, icono, textoBoton) {    
+  //   swal.fire({
+  //     title: titulo,
+  //     text: mensaje,
+  //     icon: icono,
+  //     confirmButtonText: textoBoton
+  //   })
+  // }
 }
