2bf0445636cd67ba648676b718901d0a6cb7bafe
nacardozog <nacardozog@heon.com.co>
2021-04-17 16:53:27 -0500

    Se deja el cargue del documento a mas general y reusable

--- IuAppClientes/src/app/servicios/servicio-general.service.ts
+++ IuAppClientes/src/app/servicios/servicio-general.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 // import swal from 'sweetalert2'
 @Injectable({
   providedIn: 'root'
 })
 export class ServicioGeneralService {
+  listaTipoIdentificacion: any = []
+  listaActualizar:any=[]
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+    this.cargarTipoIdentificacion()
+  }
   // MostrarModal(titulo, mensaje, icono, textoBoton) {    
   //   swal.fire({
   //     title: titulo,
@@ -14,4 +20,13 @@ export class ServicioGeneralService {
   //     confirmButtonText: textoBoton
   //   })
   // }
+  cargarTipoIdentificacion() {
+    this.http.get(environment.urlApi + '/tiposIdentificacion').subscribe(
+      (data: Array<any>) => {
+
+        this.listaTipoIdentificacion = data     
+      }, error => {
+        console.log(error);
+      });
+  }
 }
