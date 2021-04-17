595c549aa0d8a2d8ca0bff57a80f9a53224f3abe
nacardozog <nacardozog@heon.com.co>
2021-04-17 12:03:50 -0500

    Ajuste de routing

--- IuAppClientes/src/app/app-routing.module.ts
+++ IuAppClientes/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ClienteComponent } from './cliente/cliente.component';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
 
 const routes: Routes = [
-
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'clientes', component: ClienteComponent },
+  { path: 'registro', component: RegistroComponent },
 ];
 
 
