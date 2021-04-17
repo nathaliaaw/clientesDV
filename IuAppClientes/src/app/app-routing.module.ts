d2881f1350fa8504ebaa6851def6d7e69be014d5
nacardozog <nacardozog@heon.com.co>
2021-04-17 16:48:08 -0500

    Se crea nuevo componente de creacion de clientes

--- IuAppClientes/src/app/app-routing.module.ts
+++ IuAppClientes/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ClienteComponent } from './cliente/cliente.component';
+import { CrearClientesComponent } from './crear-clientes/crear-clientes.component';
 import { LoginComponent } from './login/login.component';
 import { RegistroComponent } from './registro/registro.component';
 
@@ -10,6 +11,7 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'clientes', component: ClienteComponent },
   { path: 'registro', component: RegistroComponent },
+  { path: 'crearClientes', component: CrearClientesComponent },
 ];
 
 
