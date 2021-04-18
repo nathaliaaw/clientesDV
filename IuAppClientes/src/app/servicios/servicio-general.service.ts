8aba9af2a0e68b46fce18b1cb4e7a56ef7bd3cb1
nacardozog <nacardozog@heon.com.co>
2021-04-17 22:42:26 -0500

    Ajuste de  sweetalert2 por la version

--- IuAppClientes/src/app/servicios/servicio-general.service.ts
+++ IuAppClientes/src/app/servicios/servicio-general.service.ts
@@ -1,31 +1,32 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
-import swal from 'sweetalert2'
+import Swal from 'sweetalert2'
+
 @Injectable({
   providedIn: 'root'
 })
 export class ServicioGeneralService {
   listaTipoIdentificacion: any = []
-  listaActualizar:any=[]
+  listaActualizar: any = []
+//  Swal = require('sweetalert2')
 
-  constructor(private http: HttpClient) { 
+  constructor(private http: HttpClient) {
     this.cargarTipoIdentificacion()
   }
-  MostrarModal(titulo, mensaje, icono, textoBoton) {    
-    swal.fire({
-      title: titulo,
-      text: mensaje,
-      icon: icono,
-      confirmButtonText: textoBoton
-    })
-    
+  MostrarModal(titulo, mensaje, icono, textoBoton='') {
+    Swal.fire(
+      titulo,
+      mensaje,
+      icono
+    )
+
   }
   cargarTipoIdentificacion() {
     this.http.get(environment.urlApi + '/tiposIdentificacion').subscribe(
       (data: Array<any>) => {
 
-        this.listaTipoIdentificacion = data     
+        this.listaTipoIdentificacion = data
       }, error => {
         console.log(error);
       });
